7c5f3126952b0df213a51ee76a0bfe7a32ecbd87
Ahmed Yasser <012ahmedelsayad012@gmail.com>
2026-09-20 19:52:07 +0300

    Enhance Employee class with displayinfo and Manager
    Added displayinfo method and Manager class to enhance functionality.

--- 1.dart
+++ 1.dart
@@ -3,29 +3,49 @@ class Employee {
   String? _name ;
   double? _salary ;
 
-
   Employee(String name , double salary){
-
     _name = name ;
     this.salary = salary;
-    
   }
+  
   set salary (double value){
-
     if (value >= 0 ){
       _salary = value ;
     }
   }
+
   double? get salary  => _salary;
+  String? get name =>_name;
+
+  displayinfo(){
+    print("Name : $name");
+    print("Salary : $salary");
+  }
 }
 
 
+class Manager extends  Employee {
+  Manager(super.name , super.salary , this.department);
 
+  String ? department ;
+
+  @override
+  displayinfo(){
+    print("Department : $department");
+    super.displayinfo();
+    print("-------------------");
+  }
+
+}
 
 void main (){
+Manager manager = Manager("Ali", 15000, "HR");
+manager.displayinfo();
 
-  Employee x = Employee("Ahmed", 120);
-  // بيطبع القيمة عن طريق السطر 19
-  print(x.salary);
+Employee employee = Employee('Youssef', 1000);
+
+print("Salary : ${employee.salary} \$");
+employee.salary=1500;
+print("Salary : ${employee.salary} \$");
 
 }
